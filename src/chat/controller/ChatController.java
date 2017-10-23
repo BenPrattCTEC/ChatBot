54ff4bcc344b602cba0a6c99511042f80d20154b
bcpratt2000@gmail.com <bcpratt2000@gmail.com>
2017-10-23 12:45:41 -0600

    added tests

--- src/chat/controller/ChatController.java
+++ src/chat/controller/ChatController.java
@@ -1,9 +1,12 @@
 package chat.controller;
 
+import chat.view.PopupDisplay;
+
 public class ChatController {
 	
 	public void start(){
-		
+		PopupDisplay test = new PopupDisplay();
+		test.displayQuestion("FLHAFBKHASDJFNj;kans;djksadas");
 		
 	}
 }
--- src/chat/view/PopupDisplay.java
+++ src/chat/view/PopupDisplay.java
@@ -2,15 +2,26 @@ package chat.view;
 
 
 import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
 
 public class PopupDisplay {
 	
+	private ImageIcon icon;
+	private String windowTitle;
+	
+	public PopupDisplay(){
+		
+		icon = new ImageIcon(getClass().getResource("Images/Chatbot.png"));
+		windowTitle = "Chatbot Says: ";
+		
+	}
+	
 	public void displayText(String text){
-		JOptionPane.showMessageDialog(null, text);
+		JOptionPane.showMessageDialog(null, text, windowTitle, JOptionPane.INFORMATION_MESSAGE, icon);
 	}
 	
 	public String displayQuestion(String text){
-		return JOptionPane.showInputDialog(null, text);
+		return (String)JOptionPane.showInputDialog(null, text, windowTitle, JOptionPane.PLAIN_MESSAGE, icon, null, "");
 	}
 	
 }
