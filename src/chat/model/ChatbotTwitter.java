c06574cecc212437c428adf4ea1b594a66d33aa4
bcpratt2000@gmail.com <bcpratt2000@gmail.com>
2018-03-12 13:55:39 -0600

    added twitter infastructure

--- src/chat/model/ChatbotTwitter.java
+++ src/chat/model/ChatbotTwitter.java
@@ -1,6 +1,11 @@
 package chat.model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import chat.controller.ChatbotController;
+import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
@@ -8,13 +13,38 @@ import twitter4j.TwitterFactory;
 public class ChatbotTwitter {
 	private ChatbotController controller;
 	private Twitter twitter;
+	private ArrayList<String> tweetedWords;
+	private ArrayList<Status> searchedTweets;
+	private long totalWordCount;
+	private HashMap wordsAndCount;
 	
-	public ChatbotTwitter(ChatbotController controller){
+	public ChatbotTwitter(ChatbotController controller) {
 		this.controller = controller;
 		this.twitter = TwitterFactory.getSingleton();
+		this.searchedTweets = new ArrayList<Status>();
+		this.tweetedWords = new ArrayList<String>();
+		this.wordsAndCount = new HashMap<String, Integer>();
+		
 	}
 	
-	public void tweet(String text){
+	public String getMostCommonWord(String username) {
+		
+		collectTweets(username);
+		turnStatusesToWords();
+		totalWordCount = tweetedWords.size();
+		String[] boring = createIgnoredWordArray();
+		removeBlanks();
+		trimTheBoringWords(boring);
+		generateWordCount();
+		
+		ArrayList<Map.Entry<String, Integer>> sorted = sortHashMap();
+		String mostCommonWord = sorted.get(0).getKey();
+		int maxWord = sorted.get(0).getValue();
+		
+		return mostCommonWord;
+	}
+	
+	public void tweet(String text) {
 		try {
 			twitter.updateStatus(text + "@ChatbotCTEC");
 		}
@@ -23,5 +53,52 @@ public class ChatbotTwitter {
 			controller.handleErrors(e);
 		}
 	}
+	
+	private void removeBlanks() {
+		for (int i = tweetedWords.size() - 1; i >= 0; i--) {
+			if (tweetedWords.get(i).trim().length() == 0) {
+				tweetedWords.remove(i);
+			}
+		}
+	}
+	
+	private void turnStatusesToWords() {
+		for (Status currentStatus : searchedTweets) {
+			String tweetText = currentStatus.getText();
+			tweetText = tweetText.replace("\n", " ");
+			String[] tweetWords = tweetText.split(" ");
+			for (int i = 0; i < tweetWords.length; i++) {
+				tweetedWords.add(removePunctuation(tweetWords[i]).trim());
+			}
+		}
+	}
+	
+	private void trimTheBoringWords(String[] boringWords) {
+		for (int i = tweetedWords.size() - 1; i >= 0; i--) {
+			for (int removeIndex = 0; removeIndex < boringWords.length; removeIndex++) {
+				if (tweetedWords.get(i).equalsIgnoreCase(boringWords[removeIndex])) {
+					tweetedWords.remove(i);
+					removeIndex = boringWords.length;
+				}
+			}
+		}
+	}
+	
+	private void generateWordCount() {
+		for (String word : tweetedWords) {
+			if (!wordsAndCount.containsKey(word.toLowerCase()))
+				wordsAndCount.put(word.toLowerCase(), 1);
+			else
+				wordsAndCount.replace(word.toLowerCase(), wordsAndCount.get(word.toLowerCase()) + 1);
+			
+		}
+	}
+	
+	private ArrayList<Map.Entry<String, Integer>> sortHashMap() {
+		ArrayList<Map.Entry<String, Integer>> entries = new ArrayList<Map.Entry<String, Integer>>(wordsAndCount.entrySet());
+		entries.sort((entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()));
 		
+		return entries;
+		
+	}
 }
